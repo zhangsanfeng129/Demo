2051fb2c157184ba9252fe1782e751cb7ea2f2ce
张三丰 <2474910956@qq.com>
2020-01-14 17:39:03 +0800

    新分支更改登陆页面

---
+++ HousPital/HosePital/HosePital/Controllers/RoomSController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HosePital.Models.DB;
+namespace HosePital.Controllers
+{
+    public class RoomSController : Controller
+    {
+        //
+        // GET: /RoomS/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Indexxiu()
+        {
+            HoseDBEntities db = new HoseDBEntities();
+            
+
+            return View();        
+        }
+    }
+}
